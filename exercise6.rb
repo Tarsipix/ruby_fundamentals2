e7ec22b3351bdb45c407e5802829e1dc2d6afa22
Brian Simon <tarsipix@gmail.com>
2014-03-25 16:36:53 -0400

    first commit

--- exercise6.rb
+++ exercise6.rb
@@ -6,5 +6,17 @@ def print_list(list)
 end
 print_list(grocery_list)
 grocery_list << "rice"
-puts ""
+
+puts "\n2. add rice"
 print_list(grocery_list)
+
+puts "\n3. Total number of items in list: " + grocery_list.length.to_s
+
+print "\n4. "
+if !grocery_list.include?("bananas")
+  puts "You need to pick up bananas."
+else
+  puts "You don't need to pick up bananas today"
+end
+
+puts "\n5. The second item in the list is " + grocery_list[1].to_s
