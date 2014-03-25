b20785ea4d1726bc8bf056f2ccf9b445f7e29df8
Brian Simon <tarsipix@gmail.com>
2014-03-25 15:50:29 -0400

    second commit

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,8 @@
 puts "Please enter a temperature in Farenheight: "
 temp_F = gets.chomp.to_i
 def convert_to_celsius(input_F)
-  return (input_F-32)*(5/9))
+  return (input_F-32)*(5/9)
 end
 
+temp_C = convert_to_celsius(temp_F).to_i
+puts "The temperature in Celsius is: " 
